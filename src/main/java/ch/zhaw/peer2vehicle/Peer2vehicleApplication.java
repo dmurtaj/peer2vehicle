d147a07f7ad75baf90885a2ce2a60c8b7f4b5858
dmurtaj <drin.murtaj@gmail.com>
2023-05-05 13:57:36 +0200

    first commit

---
+++ src/main/java/ch/zhaw/peer2vehicle/Peer2vehicleApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.peer2vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peer2vehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Peer2vehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/peer2vehicle/Peer2vehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.peer2vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Peer2vehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
